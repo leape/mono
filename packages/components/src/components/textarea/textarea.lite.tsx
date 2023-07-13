f20259505380892e8cc857745f9a1920e19353b9
Duncan Kolba <duncan.kolba@deutschebahn.com>
2023-07-13 17:17:30 +0200

    WIP

--- packages/components/src/components/textarea/textarea.lite.tsx
+++ packages/components/src/components/textarea/textarea.lite.tsx
@@ -43,7 +43,7 @@ export default function DBTextarea(props: DBTextareaProps) {
 			rows: '2',
 			cols: '33'
 		},
-		handleChange: (event) => {
+		handleChange: (event: any) => {
 			if (props.onChange) {
 				props.onChange(event);
 			}
