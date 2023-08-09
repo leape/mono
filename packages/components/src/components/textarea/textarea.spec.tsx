519e46bed9184aa560362465c399a328cf244aba
Duncan Kolba <duncan.kolba@deutschebahn.com>
2023-08-09 14:25:32 +0200

    WIP

--- packages/components/src/components/textarea/textarea.spec.tsx
+++ packages/components/src/components/textarea/textarea.spec.tsx
@@ -5,12 +5,12 @@ import { DBTextarea } from './index';
 // @ts-ignore - vue can only find it with .ts as file ending
 import { DEFAULT_VIEWPORT } from '../../shared/constants.ts';
 
-const comp = <DBTextarea value="Test">Test</DBTextarea>;
+const comp = <DBTextarea value="Test" label="Label"></DBTextarea>;
 
 const testComponent = () => {
-	test('should contain text', async ({ mount }) => {
+	test('Label should have Text', async ({ mount }) => {
 		const component = await mount(comp);
-		await expect(component).toContainText('Test');
+		await expect(component).toContainText('Label');
 	});
 
 	test('should match screenshot', async ({ mount }) => {
@@ -25,6 +25,7 @@ test.describe('DBTextarea', () => {
 });
 
 test.describe('DBTextarea', () => {
+	// TODO
 	test.skip('should not have any A11y issues', async ({ page, mount }) => {
 		await mount(comp);
 		const accessibilityScanResults = await new AxeBuilder({ page })
